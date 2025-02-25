4b16fee578456e5364c15ffac80e0c18571d68f3
rafeza182 <rafeza182@gmail.com>
2025-02-25 12:15:35 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,3 +3,25 @@ self.addEventListener("fetch", function(event) {
         event.respondWith(fetch(event.request));
     }
 });
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("uniradio-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/script.js",
+        "/icons/icon-192x192.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
